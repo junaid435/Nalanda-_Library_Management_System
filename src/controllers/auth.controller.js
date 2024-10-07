1d4db60b1293da78aed9bc39ec1ef2c5da0ed5ab
junaid <junaidvinu133@gmail.com>
2024-10-07 19:50:46 +0530

    feat : added login api

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+import {
+  find_user_email_repo,
+  user_register_repo,
+} from "../repositories/auth.repository.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import bcrypt from "bcrypt";
+import { apiError } from "../utils/apiError.js";
+import Jwt from "jsonwebtoken";
+
+
+const user_register = asyncHandler(async (req, res) => {
+  const { email, password, name } = req.body;
+  const salt = await bcrypt.genSalt(12);
+
+  const hashePassword = await bcrypt.hash(password.toString(), salt);
+
+  const userData = await find_user_email_repo(email);
+  if (userData) {
+    throw new apiError(409, "email already used");
+  }
+  await user_register_repo(email, name, hashePassword);
+  res
+    .status(201)
+    .json(new apiResponse(201, [], "user registered successfully"));
+});
+
+const user_login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const userData = await find_user_email_repo(email);
+  if (!userData) {
+    throw new apiError(404, "User not found");
+  }
+  const result = await bcrypt.compare(
+    password.toString(),
+    userData["password"]
+  );
+  if (!result) {
+    throw new apiError(401, "incorrect password");
+  }
+  const token = Jwt.sign(
+    {
+      email: userData["email"],
+      role: userData["is_admin"] ? "admin" : "member",
+    },
+    process.env.USER_SECRET
+  );
+  res.status(200).json(new apiResponse(200, token,'user login successfully'));
+});
+
+export { user_register, user_login };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { user_register } from "../controllers/auth.controlller.js";
+import { user_login, user_register } from "../controllers/auth.controller.js";
 
 const route = Router();
 
 route.route('/register').post(user_register)
-
+route.route('/login').post(user_login)
 export default route
