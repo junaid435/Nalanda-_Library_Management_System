0d3ebe4e40f040542ad6c182111461761406893b
junaid <junaidvinu133@gmail.com>
2024-10-08 13:07:28 +0530

    feat : add borrow book return api

--- src/controllers/borrow.controller.js
+++ src/controllers/borrow.controller.js
@@ -2,7 +2,10 @@ import {
   book_total_copies_update,
   find_book_by_id,
 } from "../repositories/book.repository.js";
-import { borrow_book_repo } from "../repositories/borrow.repository.js";
+import {
+  borrow_book_repo,
+  borrow_return_repo,
+} from "../repositories/borrow.repository.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -17,14 +20,20 @@ const borrow_book = asyncHandler(async (req, res) => {
     );
   }
   await borrow_book_repo(req);
-  await book_total_copies_update(book_data,false);
+  await book_total_copies_update(book_data, false);
 
   res.status(201).json(new apiResponse(201, [], "borrow book successfully"));
 });
 
-const borrow_return=asyncHandler((req,res)=>{
-    const {id}=req.body
-    
-})
+const borrow_return = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  let book_data = await find_book_by_id(id);
+  if (!book_data) {
+    throw new apiError(404, "The book is founded");
+  }
+  await borrow_return_repo(id);
+  await book_total_copies_update(book_data, true);
+  res.status(201).json(new apiResponse(201, [], "borrow book returned successfully"));
+});
 
-export { borrow_book,borrow_return };
+export { borrow_book, borrow_return };
--- src/repositories/borrow.repository.js
+++ src/repositories/borrow.repository.js
@@ -1,15 +1,16 @@
-import { book_model } from "../models/book.models.js"
-import { borrow_model } from "../models/borrow.models.js"
+import { book_model } from "../models/book.models.js";
+import { borrow_model } from "../models/borrow.models.js";
 
-const borrow_book_repo=(req)=>{
-    const borrow_book=new borrow_model({
-        user:req.user._id,
-        book:req.body.id
-    })
-    
-    return borrow_book.save()
-}
+const borrow_book_repo = (req) => {
+  const borrow_book = new borrow_model({
+    user: req.user._id,
+    book: req.body.id,
+  });
 
-export {
-    borrow_book_repo
-}
+  return borrow_book.save();
+};
+const borrow_return_repo = (id) => {
+  return book_model.findByIdAndUpdate({ _id: id }, { $set: { return_status: true } });
+};
+
+export { borrow_book_repo, borrow_return_repo };
